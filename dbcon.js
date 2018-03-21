ce87bc9a94d268b8d188795722b72d48032b2c76
BenjaminF <fridkisb@oregonstate.edu>
2018-03-21 17:42:26 -0500

    Demo

--- dbcon.js
+++ dbcon.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'mysql://bf57ae5b87d78d:3d33c6b9@us-cdbr-iron-east-05.cleardb.net/heroku_6ad1e967f6f86fb?reconnect=true',
+  host            : 'us-cdbr-iron-east-05.cleardb.net',
   user            : 'bf57ae5b87d78d',
   password        : '3d33c6b9',
   database        : 'heroku_6ad1e967f6f86fb'
