8c020d86a692b6bfca844d4e0f00ce8ae312bc1d
BenjaminF <fridkisb@oregonstate.edu>
2018-03-21 17:36:34 -0500

    Demo

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_fridkisb',
-  password        : '0627',
-  database        : 'cs340_fridkisb'
+  host            : 'mysql://bf57ae5b87d78d:3d33c6b9@us-cdbr-iron-east-05.cleardb.net/heroku_6ad1e967f6f86fb?reconnect=true',
+  user            : 'bf57ae5b87d78d',
+  password        : '3d33c6b9',
+  database        : 'heroku_6ad1e967f6f86fb'
 });
 
 module.exports.pool = pool;
