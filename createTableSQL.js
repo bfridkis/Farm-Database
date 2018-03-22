e51de270904ff212d4bfd0977f1993b2999e8ecb
BenjaminF <fridkisb@oregonstate.edu>
2018-03-22 11:26:32 -0500

    Demo

--- createTableSQL.js
+++ createTableSQL.js
@@ -1,9 +1,9 @@
-module.exports = "CREATE TABLE `plot` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` ENUM('Pasture', 'Nightshade', 'Legume', 'Brassica', 'Root Vegetable', 
+module.exports = "CREATE TABLE plot (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  type ENUM('Pasture', 'Nightshade', 'Legume', 'Brassica', 'Root Vegetable', 
 	'Woodland', 'Spring Wheat', 'Winter Wheat', 'Oats', 'Fruit') NOT NULL,
-  `cycle_status` int(1) NOT NULL check (`Cycle_Status` between 0 and 5),
-  `previous_type` ENUM('Pasture', 'Nightshade', 'Legume', 'Brassica', 'Root Vegetable', 
+  cycle_status int(1) NOT NULL check (Cycle_Status between 0 and 5),
+  previous_type ENUM('Pasture', 'Nightshade', 'Legume', 'Brassica', 'Root Vegetable', 
 	'Woodland', 'Spring Wheat', 'Winter Wheat', 'Oats', 'Fruit', 'N/A'),
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;"
