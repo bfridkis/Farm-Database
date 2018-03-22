c23868cf1d0a4d71f11ad08f20e3dabfc8f2af2b
BenjaminF <fridkisb@oregonstate.edu>
2018-03-22 12:48:27 -0500

    Demo

--- createTablesSQL.js
+++ createTablesSQL.js
@@ -1,5 +1,5 @@
 module.exports = function(){  
-	var createTablesSQLStatement = String("DROP TABLE IF EXISTS `employee_plot`;"); 
+	var createTablesSQLStatement = String("DROP TABLE IF EXISTS `employee_plot`;DROP TABLE IF EXISTS `equipment`;"); 
 
 	return createTablesSQLStatement;
 }
